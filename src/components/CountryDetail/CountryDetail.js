8fd73c49d65f39af716bf14fac31301a787a1211
unknown <sifatniloy18@gmail.com>
2022-08-10 22:41:20 -1200

    link style added

--- src/components/CountryDetail/CountryDetail.js
+++ src/components/CountryDetail/CountryDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const CountryDetail = () => {
-    const countryName = useParams();
+    const { countryName } = useParams();
     return (
         <div>
             <h2>Country detail for: {countryName}</h2>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CustomLink from '../CustomLink/CustomLink';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
             <h2>Welcome to my routing website!!!</h2>
             <nav>
-                <Link to="/home">Home</Link>
-                <Link to="/posts">Posts</Link>
-                <Link to="/friends">Friends</Link>
-                <Link to="/countries">Countries</Link>
-                <Link to="/About">About</Link>
+                <Link className='link' to="/home">Home</Link>
+                <Link className='link' to="/posts">Posts</Link>
+                <Link className='link' to="/friends">Friends</Link>
+                <Link className='link' to="/countries">Countries</Link>
+                <Link className='link' to="/About">About</Link>
                 {/* <CustomLink to="/">Home</CustomLink>
                 <CustomLink to="/friends">Friends</CustomLink>
                 <CustomLink to="/about">About</CustomLink> */}
